a0b280be20a96a9e38fb0d7b40f2b5f1c83e4a46
Vasilis Karvelas <vkarvelas@novibet.com>
2024-06-25 17:18:21 +0300

    all issues fixed

--- NugetCompare.Application/ApplicationModule.fs
+++ NugetCompare.Application/ApplicationModule.fs
@@ -1,16 +1,14 @@
 ﻿module CompareNugets
 
-open System.IO // Import System.IO namespace for file operations
-open System.Xml.Linq // Import System.Xml.Linq for XML processing
+open System.IO
+open System.Xml.Linq
 
 type PackageInfo = { Name: string; Version: string; ProjectFile: string }
 
-// Define a recursive function to find all .csproj files in a given path
 let rec FindAllCsprojFiles path =
-    // Search for .csproj files in all directories starting from the given path
     Directory.EnumerateFiles(path, "*.csproj", SearchOption.AllDirectories)
 
-let ExtractPackageReferences (basePath:string) (csprojPath: string) =
+let ExtractPackageReferences (basePath: string) (csprojPath: string) =
     let relativePath = csprojPath.Substring(basePath.Length).TrimStart(Path.DirectorySeparatorChar)
     let doc = XDocument.Load(csprojPath)
     doc.Descendants("PackageReference")
@@ -24,26 +22,25 @@ let ExtractPackageReferences (basePath:string) (csprojPath: string) =
         { Name = name; Version = version; ProjectFile = relativePath })
     |> Seq.toList
 
-// Create a map of package names to package details for each solution
 let CreatePackageMap (packages: seq<PackageInfo>) =
     packages
-    |> Seq.groupBy (fun pkg -> pkg.Name, pkg.Version)
-    |> Seq.map (fun (name, pkgs) -> (name, Seq.head pkgs))
+    |> Seq.groupBy (fun pkg -> (pkg.Name, pkg.ProjectFile))
+    |> Seq.map (fun ((name, projectFile), pkgs) -> ((name, projectFile), pkgs |> Seq.head))
     |> Map.ofSeq
 
-// Function to compare and format the package information
 let FormatComparison packageMap1 packageMap2 =
     let keys1 = packageMap1 |> Map.toSeq |> Seq.map fst
     let keys2 = packageMap2 |> Map.toSeq |> Seq.map fst
 
-    let commonPackageNames = Set.intersect (Set.ofSeq keys1) (Set.ofSeq keys2)
-    commonPackageNames
-    |> Seq.map (fun packageName ->
-        let package1 = Map.tryFind packageName packageMap1
-        let package2 = Map.tryFind packageName packageMap2
-        match (package1, package2) with
-        | (Some pkg1, Some pkg2) when pkg1.Version <> pkg2.Version -> sprintf "%s %s (%s) | %s %s (%s)" pkg1.Name pkg1.Version pkg1.ProjectFile pkg2.Name pkg2.Version pkg2.ProjectFile
-        | _ -> "" // This case should not occur for common packages
+    let commonPackageKeys = Set.intersect (Set.ofSeq keys1) (Set.ofSeq keys2)
+    commonPackageKeys
+    |> Seq.map (fun packageKey ->
+        let package1 = Map.find packageKey packageMap1
+        let package2 = Map.find packageKey packageMap2
+        if package1.Version <> package2.Version then
+            sprintf "%s %s (%s) | %s %s (%s)" package1.Name package1.Version package1.ProjectFile package2.Name package2.Version package2.ProjectFile
+        else
+            ""
     )
     |> Seq.filter (fun x -> x <> "")
     |> String.concat "\n"
@@ -52,24 +49,23 @@ let FormatComparisonCsv packageMap1 packageMap2 =
     let keys1 = packageMap1 |> Map.toSeq |> Seq.map fst
     let keys2 = packageMap2 |> Map.toSeq |> Seq.map fst
 
-    let commonPackageNames = Set.intersect (Set.ofSeq keys1) (Set.ofSeq keys2)
+    let commonPackageKeys = Set.intersect (Set.ofSeq keys1) (Set.ofSeq keys2)
     let comparisons =
-        commonPackageNames
-        |> Seq.map (fun packageName ->
-            let package1 = Map.tryFind packageName packageMap1
-            let package2 = Map.tryFind packageName packageMap2
-            match (package1, package2) with
-            | (Some pkg1, Some pkg2) when pkg1.Version <> pkg2.Version -> 
-                Some (pkg1.Name, pkg1.Version, pkg1.ProjectFile, pkg2.Name, pkg2.Version, pkg2.ProjectFile)
-            | _ -> None // This case should not occur for common packages
+        commonPackageKeys
+        |> Seq.choose (fun packageKey ->
+            let package1 = Map.find packageKey packageMap1
+            let package2 = Map.find packageKey packageMap2
+            if package1.Version <> package2.Version then
+                Some (package1.ProjectFile, package1.Name, package1.Version, package2.Version, package2.Name, package2.ProjectFile)
+            else
+                None
         )
-        |> Seq.choose id
     comparisons
 
 let ToCsvFormat (comparisons: seq<string * string * string * string * string * string>) =
     let csvLines = 
         comparisons 
-        |> Seq.map (fun (name1, version1, projectFile1, name2, version2, projectFile2) ->
+        |> Seq.map (fun (projectFile1, name1, version1, version2, name2, projectFile2) ->
             sprintf "\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"" projectFile1 name1 version1 version2 name2 projectFile2)
     String.concat "\n" csvLines
 
--- NugetCompare.ConsoleApp/Program.fs
+++ NugetCompare.ConsoleApp/Program.fs
@@ -1,11 +1,12 @@
 ﻿open System
-open System.IO // Import System.IO namespace for file operations
+open System.IO
 
 [<EntryPoint>]
 let main args =
     let argv =
     #if DEBUG
-        [|"C:\\Users\\vkarvelas\\Repositories\\Integrated.Dias_develop" ; "C:\\Users\\vkarvelas\\Repositories\\Integrated.Dias" ; "./output"|]
+    // your code
+        args
     #else
         args
     #endif
@@ -20,29 +21,40 @@ let main args =
 
             // Ensure the output directory exists
             let outputDir = Path.GetDirectoryName(comparisonOutputFile)
-            if not (Directory.Exists(outputDir)) then 
+            if not (String.IsNullOrWhiteSpace(outputDir) && Directory.Exists(outputDir)) then 
                 Directory.CreateDirectory(outputDir) |> ignore
 
             // Clear or create the output file
             File.WriteAllText(comparisonOutputFile, "")
 
-            // After calling findAllCsprojFiles for each solution
+            // Find all .csproj files for each solution
             let csprojFilesSolution1 = CompareNugets.FindAllCsprojFiles solutionDir1
             let csprojFilesSolution2 = CompareNugets.FindAllCsprojFiles solutionDir2
 
-            let packagesSolution1 = csprojFilesSolution1 |> Seq.collect (CompareNugets.ExtractPackageReferences solutionDir1) // |> Seq.distinct
-            let packagesSolution2 = csprojFilesSolution2 |> Seq.collect (CompareNugets.ExtractPackageReferences solutionDir2) // |> Seq.distinct
+            let packagesSolution1 = csprojFilesSolution1 |> Seq.collect (CompareNugets.ExtractPackageReferences solutionDir1)
+            let packagesSolution2 = csprojFilesSolution2 |> Seq.collect (CompareNugets.ExtractPackageReferences solutionDir2)
 
+            // Debugging information
             printfn "csproj files 1: %A" csprojFilesSolution1
+            printfn "Packages in Solution 1: %A" packagesSolution1
+            printfn "csproj files 2: %A" csprojFilesSolution2
+            printfn "Packages in Solution 2: %A" packagesSolution2
 
             let packageMap1 = CompareNugets.CreatePackageMap packagesSolution1
             let packageMap2 = CompareNugets.CreatePackageMap packagesSolution2
 
-           #if DEBUG
-            //printfn "package map 1 %A " packageMap1
-           #endif
+            // Debugging information
+            printfn "Package Map 1: %A" packageMap1
+            printfn "Package Map 2: %A" packageMap2
 
             let comparisonContent = CompareNugets.FormatComparison packageMap1 packageMap2
+            printfn "Comparison Content (Text): %s" comparisonContent
+
+            let comparisonContentCsv = CompareNugets.FormatComparisonCsv packageMap1 packageMap2
+            printfn "Comparison Content (CSV): %A" comparisonContentCsv
+
+            let csvContent = CompareNugets.ToCsvFormat comparisonContentCsv
+            printfn "CSV Content: %s" csvContent
 
             let directory1Info = DirectoryInfo(solutionDir1)
             let solutionDir1 = directory1Info.Name 
@@ -51,23 +63,28 @@ let main args =
             let solutionDir2 = directory2Info.Name 
 
             // Write the formatted comparison to the output file
-            File.AppendAllText(comparisonOutputFile, sprintf "Package Comparison between %s and %s:\n%s\n" solutionDir1 solutionDir2 comparisonContent)
-
-            let comparisonContent = CompareNugets.FormatComparisonCsv packageMap1 packageMap2
-            let csvContent = CompareNugets.ToCsvFormat comparisonContent
+            if not (String.IsNullOrEmpty(comparisonContent)) then
+                File.AppendAllText(comparisonOutputFile, sprintf "Package Comparison between %s and %s:\n%s\n" solutionDir1 solutionDir2 comparisonContent)
+            else
+                printfn "No differences found to write to the output file."
 
             let headers = sprintf "\"%s Project File\",\"%s Name\",\"%s Version\",\"%s Version\",\"%s Name\",\"%s Project File\"" solutionDir1 solutionDir1 solutionDir1 solutionDir2 solutionDir2 solutionDir2
             let fullCsv = headers + "\n" + csvContent
-            File.WriteAllText((Path.GetFileNameWithoutExtension comparisonOutputFile) + ".csv", fullCsv)
+            let csvFileName = Path.Combine(Path.GetDirectoryName(comparisonOutputFile), (Path.GetFileNameWithoutExtension comparisonOutputFile) + ".csv")
+            if not (String.IsNullOrEmpty(csvContent)) then
+                File.WriteAllText(csvFileName, fullCsv)
+            else
+                printfn "No differences found to write to the CSV file."
 
             // Print a message indicating that the comparison is complete
-            printfn "Package comparison complete. Differences are in %s and in " comparisonOutputFile
+            printfn "Package comparison complete. Differences are in %s and in %s" comparisonOutputFile csvFileName
 
-            0 // Return zero to indicate success
-            with
-            | :? UnauthorizedAccessException as ex ->
-                printfn "Error: Unauthorized access - %s" ex.Message
-                1
-            | ex ->
-                printfn "An unexpected error occurred: %s" ex.Message
-                1
+            0
+        with
+        | :? UnauthorizedAccessException as ex ->
+            printfn "Error: Unauthorized access - %s" ex.Message
+            1
+        | ex ->
+            printfn "An unexpected error occurred: %s" ex.Message
+            1
+
